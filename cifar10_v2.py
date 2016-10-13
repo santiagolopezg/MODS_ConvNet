50f7885dbc95675a824963f0c3a23977fdbea2fa
santiagolopezg <santiago.lopez.g@upch.pe>
2016-10-13 10:44:48 -0500

    trying stuff

--- cifar10_v2.py
+++ cifar10_v2.py
@@ -74,13 +74,13 @@ model = Sequential()
 model.add(Convolution2D(128, 3, 3,
                         input_shape=(img_channels, img_rows, img_cols),
 			activation='relu',
-			border_mode = 'same',
+			#border_mode = 'same',
 			init=weight_init, name='conv1_1'))
 model.add(BatchNormalization())
 
 model.add(Convolution2D(128, 3, 3,
                         activation='relu', 
-                        border_mode = 'same',
+                        #border_mode = 'same',
 			init=weight_init, name='conv1_2'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -88,19 +88,19 @@ model.add(Dropout(dropout))
 
 model.add(Convolution2D(256, 3, 3,
                         activation='relu', 
-                        border_mode = 'same',
+                        #border_mode = 'same',
 			init=weight_init, name='conv2_1'))
 model.add(BatchNormalization())
 
 model.add(Convolution2D(256, 3, 3,
                         activation='relu', 
-                        border_mode = 'same',
+                        #border_mode = 'same',
 			init=weight_init, name='conv2_2'))
 model.add(BatchNormalization())
 
 model.add(Convolution2D(256, 3, 3,
                         activation='relu',
-                        border_mode = 'same',
+                        #border_mode = 'same',
 			init=weight_init, name='conv2_3'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))  
@@ -108,13 +108,13 @@ model.add(Dropout(dropout))
 
 model.add(Convolution2D(512, 3, 3,
                         activation='relu',
-                        border_mode = 'same',
+                        #border_mode = 'same',
 			init=weight_init, name='conv3_1'))
 model.add(BatchNormalization())
 
 model.add(Convolution2D(512, 3, 3,
                         activation='relu',
-                        border_mode = 'same',
+                        #border_mode = 'same',
 			init=weight_init, name='conv3_2'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))  
@@ -122,21 +122,21 @@ model.add(Dropout(dropout))
 
 model.add(Convolution2D(1024, 3,3,
 			activation='relu',
-			border_mode='same',
+			#border_mode='same',
 			init=weight_init, name='conv4_1'))
 
 model.add(BatchNormalization())
 
 model.add(Convolution2D(1024, 3,3,
 			activation='relu',
-			border_mode='same',
+			#border_mode='same',
 			init=weight_init, name='conv4_2'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))  
 model.add(Dropout(dropout))
 
 model = Flatten()(model)
-model.add(Dense(1000, activation = 'relu'))
+model.add(Dense(1000, activation='relu'))
 model.add(Dropout(dropout))
 
 model.add(Dropout(dropout))
